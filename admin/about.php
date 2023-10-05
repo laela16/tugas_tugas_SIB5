091bc87beb412c255a71f1093132e48fd4957410
laela16 <nurlaela.nl1616@gmail.com>
2023-10-05 09:16:24 -0600

    tambahan

---
+++ admin/about.php
@@ -0,0 +1,57 @@
+<?php
+include_once 'top.php';
+include_once 'menu.php';
+
+?>
+<div class="row">
+		<div class="col-md-12">
+			<div class="jumbotron">
+			<div class="carousel">
+			<img class="d-block w-100" alt="Carousel Bootstrap Third" src="IT.png">
+				<div class="carousel-caption">
+					<h4>
+						Prodi Teknik Informatika
+					</h4>
+					<p>
+						Saya merupakan mahasiswa prodi teknik informatika asal universitas pamulang, dan saya diberi kesempatan untuk mengikuti program MSIB batch 5 Kampus merdeka pada pt Hendevane indonesia.  
+					</p>
+				</div>
+			</div>
+            <h2>Hallo, Saya Nurlaela</h2>
+            <fieldset>
+                <legend>About Me</legend>
+                    <ol type="A">
+                        <li>Pendidikan</li>
+                            <ul type = "square">
+                                <li>SDN Situgadung 2</li>
+                                <li>SMP PluS Darul Hikmah</li>
+                                <li>SMAN 17 KAB TANGERANG </li>
+                                <li>Universitas Pamulang</li>
+                            </ul>
+                        <li>Pengalaman</li>
+                            <ul type = "square">
+                                <li>Mengembangkan Sistem Informasi Absensi Siswa pada Laboratorium sekolah bersama Tim</li>
+                                <li>Merancang Aplikasi Inventory coffe shop berbasis web bersama Tim</li>
+                            </ul>
+                        <li>Hobby</li>
+                            <ul type="square">
+                                <li>Membaca</li>
+                                <li>Menonton</li>
+                                <li>Menulis</li>
+                            </ul>
+                        <li>Makanan Kesukaan</li>
+                            <ul type="square">
+                                <li>Chicken Katsu</li>
+                                <li>Nasi Goreng</li>
+                                <li>Ayam Bakar</li>
+                            </ul>
+                    </ol>
+            </fieldset>
+			</div>
+		</div>
+
+	</div>
+<?php
+include_once 'bottom.php';
+
+?>
